0b4a94d511798014e945d78cf3dcbe913e7793d9
wuj <1450626722@qq.com>
2022-04-12 11:20:44 +0800

    ✨ feat(): fix

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,13 +1,10 @@
-
 const { merge } = require("webpack-merge");
-const ESLintPlugin = require("eslint-webpack-plugin");
 
 const baseConfig = require("./webpack.base.js");
 
 module.exports = merge(baseConfig, {
   mode: "development",
   target: "web",
-  plugins: [new ESLintPlugin({ extensions: ["js", "ts", "vue"] })],
   devServer: {
     hot: true,
     open: true,
